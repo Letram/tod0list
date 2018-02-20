2c0d99043dc53ae0429c7d632b08e12bf8280e69
Carlos Martel Lamas <carlos.martel105@alu.ulpgc.es>
2018-02-20 22:11:47 +0000

    priority checkbox implemented
    Sorting by priority implemented.

--- src/app/filtro-done.pipe.ts
+++ src/app/filtro-done.pipe.ts
@@ -6,14 +6,28 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FiltroDonePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    const done = args;
-    console.log("Value: ", value);
-    if(!done)return value;
+  transform(value: any, mostrarPendientes, mostrarPrioridad): any {
+    console.log("Pendientes? ", mostrarPendientes);
+    console.log("Por prioridad? ", mostrarPrioridad);
+    const done = mostrarPendientes;
+    if(!done){
+      if(mostrarPrioridad){
+        return value.sort(function(a, b){
+          return -(a.prioridad - b.prioridad);
+        });
+      }else return value;
+    }
     if(!value)return null;
     //como queremos hacer un filtro para las cosas que están completas o no, a cada elemento del array value (cada elemento se llama item)
     //hay un return implicito en el item-done (siempre será true o false.)
     let filtro = value.filter(item => item.done==!done);
+    if(mostrarPrioridad){
+      console.log("Antes de la prioridad: ", filtro);
+      filtro = filtro.sort(function(a, b){
+        return -(a.prioridad - b.prioridad);
+      });
+      console.log("Después de la ordenación: ", filtro);
+    }
     return filtro;
   }
 
